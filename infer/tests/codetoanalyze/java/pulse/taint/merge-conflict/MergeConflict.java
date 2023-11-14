0a2f475a9e250cacf41a9c812e9dfde5544b65e4
Gabriela Cunha Sampaio <gabrielasampaio@fb.com>
2023-11-14 09:53:02 -0300

    Merge conflict example

---
+++ infer/tests/codetoanalyze/java/pulse/taint/merge-conflict/MergeConflict.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class Text {
+
+  public String text;
+
+  void cleanText() {
+    //conflict!
+    this.normalizeWhitespace();
+    this.removeComments();
+    this.removeDuplicateWords();
+  }
+
+  // Contribution 1
+  void normalizeWhitespace() {
+    // removes duplicate whitespaces
+    this.text = this.text.trim().replaceAll(" +", " ");
+  }
+
+  void removeComments() {}
+
+  // Contribution 2
+  void removeDuplicateWords() {
+    // removes duplicate words without normalizing whitespaces
+    String[] words = this.text.split("\\s+");
+    Stack<String> st = new Stack<>();
+    for (int i = 0; i < words.length; i++) {
+      if (st.isEmpty() || st.peek() != words[i]) {
+        st.push(words[i]);
+      } else {
+        st.push(" ");
+      }
+    }
+    this.text = String.join(" ", st);
+  }
+}
